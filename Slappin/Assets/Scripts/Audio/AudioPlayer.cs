49337d1c7c5c2a09dd0a775c9f3040b0bbb1ec60
Casey Weeks <huminaboz@gmail.com>
2024-10-14 23:38:36 -0300

    Updated the Audioplayer to allow more sfx to play at once

--- Slappin/Assets/Scripts/Audio/AudioPlayer.cs
+++ Slappin/Assets/Scripts/Audio/AudioPlayer.cs
@@ -1,29 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Serialization;
 
 public abstract class AudioPlayer : MonoBehaviour
 {
     // size of the pool of audio sources to keep consistently once created
-    [SerializeField] private int maxSimultaneousSFXs = 4;
+    [SerializeField] private int maxSimultaneousSFXs = 5;
 
-    protected List<AudioClip> playingAudioClips = new List<AudioClip>();
+    //Needed in case I need to stop the clips from playing
+    private List<AudioClip> _playingAudioClips = new List<AudioClip>();
+    
     [SerializeField] protected AudioMixerGroup audioGroup;
 
-    private List<AudioSource> sourcesNotInUse = new List<AudioSource>();
-    private List<AudioSource> sourcesInUse = new List<AudioSource>();
-
+    private List<AudioSource> _sourcesNotInUse = new List<AudioSource>();
+    private List<AudioSource> _sourcesInUse = new List<AudioSource>();
 
     protected AudioSource DoPlay(SFXScrob sfxScrob)
     {
         //First, clean up any unused AudioSources
-        for (int i = sourcesInUse.Count - 1; i > -1; i--)
+        for (int i = _sourcesInUse.Count - 1; i > -1; i--)
         {
-            if (sourcesInUse[i].isPlaying) continue;
-            sourcesNotInUse.Add(sourcesInUse[i]);
-            playingAudioClips.Remove(sourcesInUse[i].clip);
-            sourcesInUse.RemoveAt(i);
+            if (_sourcesInUse[i].isPlaying) continue;
+            if (ExceedingCapacity())
+            {
+                Destroy(_sourcesInUse[i]);
+            }
+            else _sourcesNotInUse.Add(_sourcesInUse[i]);
+            _playingAudioClips.Remove(_sourcesInUse[i].clip);
+            _sourcesInUse.RemoveAt(i);
         }
         
         if (sfxScrob.clip is null)
@@ -32,22 +36,15 @@ public abstract class AudioPlayer : MonoBehaviour
             return null;
         }
 
-        AudioSource source;
-        //Then see if the requested scrob is already playing and use that again
-        // if (playingAudioClips.Contains(sfxScrob.clip))
-        // {
-        //     source = GetSourceWithSameClip(sfxScrob.clip);
-        // }
-        // else 
-            source = GetFreeAudioSource();
-        if (source is null) return null;
+        AudioSource source = GetFreeAudioSource();
 
         source.outputAudioMixerGroup = sfxScrob.mixerGroup;
         if (source.outputAudioMixerGroup == null) source.outputAudioMixerGroup = audioGroup;
 
+        source.pitch = 1f; //reset the pitch
         if (sfxScrob.varyPitch)
         {
-            source.pitch = Random.Range(.9f, 1f);
+            source.pitch = 1f + Random.Range(-.06f, .06f);
         }
 
         source.clip = sfxScrob.clip;
@@ -55,57 +52,47 @@ public abstract class AudioPlayer : MonoBehaviour
         source.loop = sfxScrob.looping;
 
         source.Play();
-        playingAudioClips.Add(sfxScrob.clip);
+        _playingAudioClips.Add(sfxScrob.clip);
         return source;
     }
 
     private bool ExceedingCapacity()
     {
-        return sourcesInUse.Count > maxSimultaneousSFXs;
+        return _sourcesInUse.Count > maxSimultaneousSFXs;
     }
 
-    private AudioSource GetSourceWithSameClip(AudioClip clip)
-    {
-        foreach (AudioSource audioSource in sourcesInUse)
-        {
-            if (audioSource.clip == clip)
-            {
-                return audioSource;
-            }
-        }
-
-        Debug.Log("Couldn't find an audiosource with the same clip, despite knowing the list of audioscources contains it");
-        return null;
-    }
+    // private AudioSource GetSourceWithSameClip(AudioClip clip)
+    // {
+    //     foreach (AudioSource audioSource in sourcesInUse)
+    //     {
+    //         if (audioSource.clip == clip)
+    //         {
+    //             return audioSource;
+    //         }
+    //     }
+    //
+    //     Debug.Log("Couldn't find an audiosource with the same clip, despite knowing the list of audioscources contains it");
+    //     return null;
+    // }
 
     protected AudioSource GetFreeAudioSource()
     {
-        //If there are too many
-        if (ExceedingCapacity()) return null;
-
-        //There are too few
-        if (sourcesNotInUse.Count < 1)
+        //If there are too few - spawn a new one
+        if (_sourcesNotInUse.Count < 1)
         {
             AudioSource newSource = gameObject.AddComponent<AudioSource>();
-            sourcesNotInUse.Add(newSource);
+            _sourcesNotInUse.Add(newSource);
         }
 
         //Finally, get a free source from the bottom
-        int index = sourcesNotInUse.Count - 1;
-        AudioSource nextSource = sourcesNotInUse[index];
-        sourcesInUse.Add(nextSource);
-        sourcesNotInUse.RemoveAt(index);
+        int index = _sourcesNotInUse.Count - 1;
+        AudioSource nextSource = _sourcesNotInUse[index];
+        _sourcesInUse.Add(nextSource);
+        _sourcesNotInUse.RemoveAt(index);
 
         return nextSource;
     }
 
 
-    protected void DoStop(AudioScrob audioScrob)
-    {
-        foreach (AudioSource src in sourcesInUse)
-        {
-            if (src.clip == audioScrob.clip)
-                src.Stop();
-        }
-    }
+
 }
