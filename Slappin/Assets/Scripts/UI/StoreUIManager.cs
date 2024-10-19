c347984343967975cde928e90d2403287fc0073f
Casey Weeks <huminaboz@gmail.com>
2024-10-18 23:06:08 -0300

    Upgrades are now automatically sorted by category with a category label for each row

--- Slappin/Assets/Scripts/UI/StoreUIManager.cs
+++ Slappin/Assets/Scripts/UI/StoreUIManager.cs
@@ -12,15 +12,17 @@ public class StoreUIManager : MonoBehaviour
     [SerializeField] private GameObject rowsParent;
     [SerializeField] private GameObject categoryRowPrefab;
     [SerializeField] private UpgradeData upgradeDataPrefab;
-    
+
     [SerializeField] private TextMeshProUGUI totalCurrencyText;
     [SerializeField] private TextMeshProUGUI nextWaveButtonText;
     [SerializeField] private Button nextWaveButton;
 
-    
+    [SerializeField] private GameObject categoryRowTitlePrefab;
+
     public delegate void DebugUpdateStoreUI();
+
     public static event DebugUpdateStoreUI OnDebugUpdateStoreUI;
-    
+
     //TODO:: Populate all the categories from the resources folder scrobs
 
     private void OnEnable()
@@ -51,16 +53,43 @@ public class StoreUIManager : MonoBehaviour
         // totalCurrencyText.text = PlayerStats.I.currency1.ToString("F0");
     }
 
+    Dictionary<UpgradeType, GameObject> categories = new Dictionary<UpgradeType, GameObject>();
+    List<UpgradeType> upgradeTypes = new List<UpgradeType>();
+
+    private Transform GetCategoryParent(UpgradeType upgradeType)
+    {
+        if (categories.TryGetValue(upgradeType, out GameObject categoryRow))
+        {
+            return categoryRow.transform;
+        }
+
+        Debug.LogError("No category found for upgrade type: " + upgradeType);
+        return null;
+    }
+
     private void BuildCategories()
     {
-        GameObject categoryRow = Instantiate(categoryRowPrefab, rowsParent.transform);
         string resourcePath = "UpgradeScrobs";
         SO_Upgrade[] upgrades = Resources.LoadAll<SO_Upgrade>(resourcePath);
+
+        foreach (SO_Upgrade upgradeSO in upgrades)
+        {
+            if (upgradeTypes.Contains(upgradeSO.upgradeType)) continue;
+            upgradeTypes.Add(upgradeSO.upgradeType);
+            GameObject categoryRow = Instantiate(categoryRowPrefab, rowsParent.transform);
+            categories.Add(upgradeSO.upgradeType, categoryRow);
+
+            //Add the category title
+            GameObject categoryRowTitle = Instantiate(categoryRowTitlePrefab, categoryRow.transform);
+            TextMeshProUGUI categoryRowTitleText = categoryRowTitle.GetComponentInChildren<TextMeshProUGUI>();
+            categoryRowTitleText.text = upgradeSO.upgradeType.ToString();
+        }
 
         // Iterate through each SO_Upgrade and instantiate an UpgradeData prefab for each
         foreach (SO_Upgrade upgradeSO in upgrades)
         {
-            UpgradeData upgradeData = Instantiate(upgradeDataPrefab, categoryRow.transform);
+            UpgradeData upgradeData = Instantiate(upgradeDataPrefab, 
+                GetCategoryParent(upgradeSO.upgradeType));
 
             // Assign the SO_Upgrade to the upgradeData's upgradeSO field
             if (upgradeData != null)
