398419ec53ed001f0b0af6ddc815fcc9149d6ac1
Casey Weeks <huminaboz@gmail.com>
2024-10-22 18:43:48 -0300

    Hp bar changes colors depending on how much hp you have

--- Slappin/Assets/Scripts/UI/HpBar.cs
+++ Slappin/Assets/Scripts/UI/HpBar.cs
@@ -7,6 +7,11 @@ public class HpBar : Singleton<HpBar>
     private Slider _hpBar;
     [SerializeField] private TextMeshProUGUI hpText;
 
+    [SerializeField] private Color defaulthpBarColor;
+    [SerializeField] private Color dangerColor;
+
+    [SerializeField] private Image hpFillImage;
+
     private void Awake()
     {
         _hpBar = GetComponent<Slider>();
@@ -15,7 +20,18 @@ public class HpBar : Singleton<HpBar>
     public void UpdateHpBar(Health health)
     {
         if (!health) return;
-        hpText.text = health.hp.ToString();
+        int hp = Mathf.Clamp(health.hp, 0, health.hp);
+        hpText.text = hp.ToString();
         _hpBar.value = (float)health.hp / health.maxHp;
+        if (_hpBar.value <= .25f)
+        {
+            hpFillImage.color = dangerColor;
+            hpText.color = dangerColor;
+        }
+        else
+        {
+            hpFillImage.color = defaulthpBarColor;
+            hpText.color = defaulthpBarColor;
+        }
     }
 }
