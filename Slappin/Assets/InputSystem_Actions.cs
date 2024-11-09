bb225943e7f89c5289541ff49cc415148f00dca5
Casey Weeks <huminaboz@gmail.com>
2024-11-09 01:59:43 -0300

    Updated the input system to work correctly for UI stuff in the store

--- Slappin/Assets/InputSystem_Actions.cs
+++ Slappin/Assets/InputSystem_Actions.cs
@@ -693,7 +693,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Submit"",
                     ""type"": ""Button"",
                     ""id"": ""7607c7b6-cd76-4816-beef-bd0341cfe950"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
